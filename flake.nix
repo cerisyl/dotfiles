35e39d024b04a97706d05f7dfdfa4a5f0b8969a7
Spencer Gunning <swcgunning@gmail.com>
2025-04-17 19:33:38 -0500

    Remove theme/extra defs from flake (oops)

--- flake.nix
+++ flake.nix
@@ -14,16 +14,9 @@
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, themeDir, extraDir, ... }@inputs:
   let
     system = "x86_64-linux";
-    themeFile = "./extra/theme";
-    theme = if builtins.pathExists themeFile
-            then builtins.readFile themeFile
-            else "ceres";
     defHost = hostname: nixpkgs.lib.nixosSystem {
       specialArgs = {
         myHostname = hostname;
-        inherit theme;
-        themeDir = "./.nix/themes/${theme}";
-        extraDir = "./.nix/extra";
         pkgsUnstable = import nixpkgs-unstable {
           inherit system;
           config.allowUnfree = true;
