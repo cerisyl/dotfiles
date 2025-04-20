6e0fa71944af3b58bcc5ebe7c58595dbd48e7bbf
Spencer Gunning <swcgunning@gmail.com>
2025-04-19 22:25:33 -0500

    Try another dynamic loader

--- nixos/config/wm/autostart.nix
+++ nixos/config/wm/autostart.nix
@@ -1,26 +1,16 @@
 # Automatically reads in anything in extra/autostart
 { config, pkgs, lib, ... }: let
   baseDir = ../../../extra/autostart;
-  allFiles = builtins.attrNames (builtins.readDir baseDir);
-  allPaths = builtins.map (name: "${baseDir}/${name}") allFiles;
+  filenames = builtins.filter (name:
+    name != "." && name != ".." && lib.hasSuffix ".desktop" name
+  ) (builtins.attrNames (builtins.readDir autostartDir));
 
-  validFiles = builtins.filter (file:
-    let
-      name = baseNameOf file;
-    in
-      name != "." && name != ".." && lib.hasSuffix ".desktop" name
-  ) allPaths;
-
-  autostartFiles = builtins.listToAttrs (builtins.map (file:
-    let
-      name = baseNameOf file;
-    in {
-      name = "autostart/${name}";
-      value = {
-        text = builtins.readFile file;
-      };
-    }
-  ) validFiles);
+  autostartFiles = builtins.listToAttrs (builtins.map (name: {
+    name = "autostart/${name}";
+    value = {
+      text = builtins.readFile (autostartDir + "/${name}");
+    };
+  }) filenames);
 in {
   xdg.configFile = autostartFiles;
 }
