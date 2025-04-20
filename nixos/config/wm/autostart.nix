f9e56a40a738bb8c5942c04b9d417406557a9b4f
Spencer Gunning <swcgunning@gmail.com>
2025-04-19 22:04:49 -0500

    Add autostarts dynamically

--- nixos/config/wm/autostart.nix
+++ nixos/config/wm/autostart.nix
@@ -1,9 +1,16 @@
-{ config, pkgs, lib, ... }: {
-  xdg.autostart = {
-    enable = true;
-    entries = [
-      "../../../extra/autostart/greenclip.desktop"
-      "../../../extra/autostart/keepassxc.desktop"
-    ];
-  };
+# Automatically reads in anything in extra/autostart
+{ config, pkgs, lib, ... }: let
+  autostartFiles = builtins.listToAttrs (map (file:
+    let
+      name = baseNameOf file;
+    in {
+      name = "autostart/${name}";
+      value.text = builtins.readFile file;
+    }) (builtins.filter
+      (f: baseNameOf f != "." && baseNameOf f != "..")
+      (builtins.attrNames (builtins.readDir ../../extra/autostart))
+      |> map (f: ../../../extra/autostart + "/${f}")
+    ));
+in {
+  xdg.configFile = autostartFiles;
 }
