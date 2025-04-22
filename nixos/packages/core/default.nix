9ebdeffdf9407e230e9d48add14816c39f56f840
Spencer Gunning <swcgunning@gmail.com>
2025-04-21 20:33:46 -0500

    Use stable rustdesk, unstable is broken

--- nixos/packages/core/default.nix
+++ nixos/packages/core/default.nix
@@ -6,7 +6,7 @@ in [
   (p "111" true   "keepassxc")
   (p "111" true   "kitty")
   (p "111" true   "peazip") # replaced engrampa
-  (p "111" true   "rustdesk")
+  (p "111" false  "rustdesk")
   (p "111" true   "syncthing")
   (p "111" true   "syncthingtray")
   #(p "111" false  "xnviewmp") needs an alternative
