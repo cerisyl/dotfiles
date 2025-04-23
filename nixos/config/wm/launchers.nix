7f28f902c61f95e86b2f198135689d3e1409641c
Spencer Gunning <swcgunning@gmail.com>
2025-04-23 18:52:09 -0500

    Trying name/exec pairs

--- nixos/config/wm/launchers.nix
+++ nixos/config/wm/launchers.nix
@@ -1,37 +1,38 @@
 { config, pkgMap, theme, getThemeFile, lib, ... }: let
+  l = name: exec: { inherit name exec; };
   removeLaunchers = [
-    "btop++"
-    "cups"
-    "kitty"
-    "micro"
-    "nixos-help"
-    "pavucontrol"
-    "rofi"
-    "rofi-theme-selector"
-    "thunar-bulk-rename"
-    "thunar-settings"
-    "thunar-volman-settings"
-    "xfce4-about"
-    "xfce4-accessibility-settings"
-    "xfce4-appearance-settings"
-    "xfce4-color-settings"
-    "xfce4-keyboard-settings"
-    "xfce4-mime-settings"
-    "xfce4-mouse-settings"
-    "xfce4-notifyd-settings"
-    "xfce4-panel"
-    "xfce4-session-logout"
-    "xfce4-session-settings"
-    "xfce4-screensaver-preferences"
-    "xfdesktop-settings"
-    "xfwm4-settings"
-    "xfwm4-tweaks-settings"
-    "xfwm4-workspace-settings"
-    "xdg-email"
-    "xdg-open"
+    (l "btop++"                 "btop")
+    (l "Manage Printing"        "cups")
+    (l "Kitty"                  "kitty")
+    (l "Micro"                  "micro")
+    (l "NixOS Manual"           "nixos-help")
+    (l "Volume Control"         "pavucontrol")
+    (l "Rofi"                   "rofi")
+    (l "Rofi Theme Selector"    "rofi-theme-selector")
+    (l "Bulk Rename"            "thunar-bulk-rename")
+    (l "File Manager Settings"  "thunar-settings")
+    (l "About Xfce"             "xfce4-about")
+    (l "Accessibility"          "xfce4-accessibility-settings")
+    (l "Appearance"             "xfce4-appearance-settings")
+    (l "Color Profiles"         "xfce4-color-settings")
+    (l "Keyboard"               "xfce4-keyboard-settings")
+    (l "Default Applications"   "xfce4-mime-settings")
+    (l "Mouse and Touchpad"     "xfce4-mouse-settings")
+    (l "Notifications"          "xfce4-notifyd-settings")
+    (l "Panel"                  "xfce4-panel")
+    (l "Logout"                 "xfce4-session-logout")
+    (l "Session and Startup"    "xfce4-session-settings")
+    (l "Xfce Screensaver"       "xfce4-screensaver-preferences")
+    (l "Desktop"                "xfdesktop-settings")
+    (l "Window Manager"         "xfwm4-settings")
+    (l "Window Manager Tweaks"  "xfwm4-tweaks-settings")
+    (l "Workspaces"             "xfwm4-workspace-settings")
+    (l "Mail Reader"            "xdg-email")
+    (l "Web Browser"            "xdg-open")
   ];
-  mappedLaunchers = builtins.listToAttrs (map (name: {
-    inherit name;
+  mappedLaunchers = builtins.listToAttrs (map (entry: {
+    name = entry.name;
+    exec = entry.exec;
     value.noDisplay = true;
   }) removeLaunchers);
 in {
