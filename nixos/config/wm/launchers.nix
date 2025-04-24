0bab1d26e068cfd052afc7436f73373a8107d9f9
Spencer Gunning <swcgunning@gmail.com>
2025-04-23 21:09:43 -0500

    Force no exec

--- nixos/config/wm/launchers.nix
+++ nixos/config/wm/launchers.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, pkgMap, theme, getThemeFile, lib, ... }: let
-  # List of launchers to remove
+{ config, pkgMap, theme, getThemeFile, lib, ... }: let
   removeLaunchers = [
     "btop"
     "cups"
@@ -9,13 +8,26 @@
     "org.pulseaudio.pavucontrol"
     "panel-preferences"
     "peazip"
+    "rofi"
+    "rofi-theme-selector"
     "thunar"
     "thunar-bulk-rename"
     "thunar-settings"
     "syncthing-ui"
     "winetricks"
     "xfce4-about"
+    "xfce4-accessibility-settings"
+    "xfce4-appearance-settings"
     "xfce-backdrop-settings"
+    "xfce4-color-settings"
+    "xfce-keyboard-settings"
+    "xfce4-mail-reader"
+    "xfce4-mime-settings"
+    "xfce-mouse-settings"
+    "xfce4-notifyd-config"
+    "xfce4-session-logout"
+    "xfce-session-settings"
+    "xfce4-settings-editor"
     "xfce4-screensaver-preferences"
     "xfce-ui-settings"
     "xfce4-web-browser"
@@ -28,14 +40,11 @@
     exec = name;
     value = {
       inherit name;
+      exec = "";
       noDisplay = true;
     };
   }) removeLaunchers);
 in {
-  # List of new launchers to create
   xdg.desktopEntries = {
-    xfce4-notifyd-config.noDisplay  = true;
-    rofi.noDisplay                  = true;
-    rofi-theme-selector.noDisplay   = true;
   } // mappedLaunchers;
 }
