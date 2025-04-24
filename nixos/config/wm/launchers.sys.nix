6151ff6cff227c93da4d01ecdcff7e3797dc3ace
Spencer Gunning <swcgunning@gmail.com>
2025-04-23 21:03:44 -0500

    Remember that .desktop exists

--- nixos/config/wm/launchers.sys.nix
+++ nixos/config/wm/launchers.sys.nix
@@ -1,17 +1,16 @@
 { config, pkgs, pkgMap, theme, getThemeFile, lib, ... }: {
   # XFCE launcher overrides (for things that aren't working in launchers.nix)
   environment.etc = {
-    "xdg/applications/xfce4-color-settings".text  = "";
-    "xdg/applications/xfce-keyboard-settings".text  = "";
-    "xdg/applications/xfce4-mail-reader".text = "";
-    "xdg/applications/xfce4-accessibility-settings".text  = "";
-    "xdg/applications/xfce4-appearance-settings".text = "";
-    "xdg/applications/xfce4-mime-settings".text = "";
-    "xdg/applications/xfce-mouse-settings".text = "";
-    "xdg/applications/xfce4-notifyd-config".text  = "";
-    "xdg/applications/xfce4-session-logout".text  = "";
-    "xdg/applications/xfce-session-settings".text = "";
-    "xdg/applications/xfce4-settings-editor".text = "";
+    "xdg/applications/xfce4-color-settings.desktop".text  = "";
+    "xdg/applications/xfce-keyboard-settings.desktop".text  = "";
+    "xdg/applications/xfce4-mail-reader.desktop".text = "";
+    "xdg/applications/xfce4-accessibility-settings.desktop".text  = "";
+    "xdg/applications/xfce4-appearance-settings.desktop".text = "";
+    "xdg/applications/xfce4-mime-settings.desktop".text = "";
+    "xdg/applications/xfce-mouse-settings.desktop".text = "";
+    "xdg/applications/xfce4-notifyd-config.desktop".text  = "";
+    "xdg/applications/xfce4-session-logout.desktop".text  = "";
+    "xdg/applications/xfce-session-settings.desktop".text = "";
+    "xdg/applications/xfce4-settings-editor.desktop".text = "";
   };
-
 }
