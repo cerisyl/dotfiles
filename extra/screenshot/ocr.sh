f4226cbab60bcc4ba32e6216ca38bc5d168a7f89
Spencer Gunning <swcgunning@gmail.com>
2025-04-27 19:17:19 -0500

    Honestly quite incredible

--- extra/screenshot/ocr.sh
+++ extra/screenshot/ocr.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # Extract text from a partial-area screenshot using OCR
 
-$fname="/tmp/ocr.png"
+# Define temporary filename location
+fname="/tmp/ocr.png"
 
+# Take the screeshot and save it to the directory
 xfce4-screenshooter -r -s $fname
-$output = $(tesseract $fname -)
-echo $output | xclip -sel clip
-
-# Display a notification that the text was copied
 
 # Copy to clipboard
 # Display a notification that the image was copied
 if [ -f $fname ]; then
-  xclip -selection clipboard -t image/png -i $fname
+  $output="$(tesseract $fname -)"
+  echo $output | xclip -sel clip
   notify-send -i clipit-trayicon -t 5000 "Copied text" "$output"
 fi
 
-# Remove the 
+# Remove the file
 rm $fname
