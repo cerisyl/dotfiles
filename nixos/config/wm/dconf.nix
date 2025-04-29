d7d0d86869a173a357bae363f7c1bdb674385e1e
Spencer Gunning <swcgunning@gmail.com>
2025-04-29 00:14:17 -0500

    Correct dconf error

--- nixos/config/wm/dconf.nix
+++ nixos/config/wm/dconf.nix
@@ -1,5 +1,7 @@
 { config, pkgMap, theme, getThemeFile, lib, ... }: {
   dconf.settings = {
-    "/org/gnome/desktop/interface/gtk-enable-primary-paste" = "false";
+    "/org/gnome/desktop/interface/gtk-theme"                = "${theme}-main";
+    "/org/gnome/desktop/interface/icon-theme"               = "${theme}-icons";
+    "/org/gnome/desktop/interface/gtk-enable-primary-paste" = false;
   };
 }
