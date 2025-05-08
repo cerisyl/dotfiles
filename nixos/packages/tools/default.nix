c5113ce720621e3281336d88831b4fc427629bf0
Spencer Gunning <swcgunning@gmail.com>
2025-05-08 00:38:05 -0500

    Add rofi theme for luna, bcompare -> meld

--- nixos/packages/tools/default.nix
+++ nixos/packages/tools/default.nix
@@ -1,7 +1,6 @@
 # General tools and utilities; typically lesser-used.
 let p = init: isUnstable: pkg: { inherit init isUnstable pkg; };
 in [
-  (p "011" false  "bcompare")
   (p "011" true   "discordchatexporter-desktop")
   (p "011" false  "ffmpeg")
   (p "011" false  "freac")
@@ -9,6 +8,7 @@ in [
   (p "111" false  "imagemagick")
   (p "001" false  "imgbrd-grabber")
   #(p "011" false  "libreoffice-fresh") rethink this one?
+  (p "011" false  "meld")
   #(p "011" false  "nine-or-null") manual install
   #(p "001" false  "ntsc-rs") manual install
   (p "011" true   "obsidian")
