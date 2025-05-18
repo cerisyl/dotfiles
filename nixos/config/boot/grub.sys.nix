51e9b971fe5fdee2942799d7d87071c0db6dace2
Spencer Gunning <csit@cering.dev>
2025-05-18 01:53:12 -0500

    Remove stray var

--- nixos/config/boot/grub.sys.nix
+++ nixos/config/boot/grub.sys.nix
@@ -20,7 +20,6 @@ in {
       "quiet" "splash" "boot.shell_on_fail" "loglevel=3"
       "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"
     ] // passthrough;
-    extraModprobeConfig
     consoleLogLevel = 0;
     initrd.verbose = false;
     loader = {
