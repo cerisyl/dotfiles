30240a40c58a26de4d9e3da634a5705ef93a3c49
Spencer Gunning <csit@cering.dev>
2025-05-18 01:56:59 -0500

    Syntax fix

--- nixos/config/boot/grub.sys.nix
+++ nixos/config/boot/grub.sys.nix
@@ -19,7 +19,7 @@ in {
     kernelParams = [
       "quiet" "splash" "boot.shell_on_fail" "loglevel=3"
       "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"
-    ] // passthrough;
+    ] ++ passthrough;
     consoleLogLevel = 0;
     initrd.verbose = false;
     loader = {
