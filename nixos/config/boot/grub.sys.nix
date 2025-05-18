e7b252f93ee62cc9b65d4560b063567c7cf2ad58
Spencer Gunning <csit@cering.dev>
2025-05-18 01:52:38 -0500

    Attempt adding passthrough to grub config

--- nixos/config/boot/grub.sys.nix
+++ nixos/config/boot/grub.sys.nix
@@ -1,6 +1,26 @@
-{ config, pkgMap, theme, getThemeFile, lib, ... }: {
+{ config, pkgMap, theme, getThemeFile, myHostname, lib, ... }: let
+  passthrough = if myHostname == lux
+    then [ "intel_iommu=on" "immou=pt" ''vfio-pci.ids="10de:2482,10de:228b"'' ]
+    else [];
+  passthroughExtra = if myHostname == lux then {
+    kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ];
+    blacklistedKernelModules = [ "nouveau" ];
+    modprobeConfig = ''
+      vfio-pci ids=10de:2482,10de:228b
+      softdep nvidia pre: vfio-pci
+    '';
+  } else {
+    kernelModules = [];
+    blacklistedKernelModules = [];
+    modprobeConfig = "";
+  };
+in {
   boot = {
-    kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
+    kernelParams = [
+      "quiet" "splash" "boot.shell_on_fail" "loglevel=3"
+      "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"
+    ] // passthrough;
+    extraModprobeConfig
     consoleLogLevel = 0;
     initrd.verbose = false;
     loader = {
@@ -18,5 +38,5 @@
         efiInstallAsRemovable = true;
       };
     };
-  };
+  } // passthroughExtra;
 }
