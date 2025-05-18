dcc6a3e1611928759c3b747655adef63b4454d11
Spencer Gunning <csit@cering.dev>
2025-05-18 13:12:20 -0500

    Remove stray var

--- nixos/config/boot/grub.sys.nix
+++ nixos/config/boot/grub.sys.nix
@@ -19,7 +19,6 @@
   };
 in {
   boot = {
-    kernelParams = passthrough;
     kernelParams = [
       "quiet" "splash" "boot.shell_on_fail" "loglevel=3"
       "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"
