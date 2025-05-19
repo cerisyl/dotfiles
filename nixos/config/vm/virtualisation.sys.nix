440704de6e90613b4e066897435ab5b14a6e7083
Spencer Gunning <csit@cering.dev>
2025-05-18 22:09:48 -0500

    Add cgroup_device_acl config

--- nixos/config/vm/virtualisation.sys.nix
+++ nixos/config/vm/virtualisation.sys.nix
@@ -6,6 +6,15 @@
         swtpm.enable = true;
         ovmf.enable = true;
         ovmf.packages = [ pkgs.OVMFFull.fd ];
+        verbatimConfig = ''
+          cgroup_device_acl = [
+            "/dev/kvm", "/dev/kvmfr0", "/dev/kvmfr1", "/dev/kvmfr2",
+            "/dev/shm/looking-glass",
+            "/dev/null", "/dev/full", "/dev/zero", "/dev/random",
+            "/dev/urandom", "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
+            "/dev/rtc", "/dev/hpet", "/dev/vfio/vfio"
+          ]
+        '';
       };
     };
     spiceUSBRedirection.enable = true;
