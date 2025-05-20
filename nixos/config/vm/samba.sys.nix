ae2fcc5717d5cb98c31d6ee45779fb356b52240d
Spencer Gunning <csit@cering.dev>
2025-05-19 21:13:38 -0500

    Format samba setup a bit

--- nixos/config/vm/samba.sys.nix
+++ nixos/config/vm/samba.sys.nix
@@ -1,22 +1,22 @@
 { config, lib, myHostname, ... }:
 if myHostname == "lux" then {
   services.samba = {
-    enable = true;
-    securityType = "user";
-    extraConfig = ''
+    enable        = true;
+    securityType  = "user";
+    extraConfig   = ''
       hosts allow = 192.168.122.0/24 127.0.0.1
       interfaces = lo virbr0
       bind interfaces only = yes
     '';
     shares = {
-      shared = {
-        path = "/home/ceri";
-        browseable = yes;
-        writable = yes;
-        "guest ok" = no;
-        "valid users" = "ceri";
-        "create mask" = "0644";
-        "directory mask" = "0755";
+      main = {
+        path              = "/home/ceri";
+        browseable        = "yes";
+        writeable         = "yes";
+        "guest ok"        = no;
+        "valid users"     = "ceri";
+        "create mask"     = "0644";
+        "directory mask"  = "0755";
       };
     };
   };
