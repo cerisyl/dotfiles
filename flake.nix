8846563514f0417a7af2d7e069e0cc31e1b9a429
Spencer Gunning <csit@cering.dev>
2025-05-27 11:39:38 -0500

    Update to 25.05 (w/ extra graphics changes)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     zmod = {
       url = "github:zarzob/Simply-Love-SM5/itgmania-release";
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
