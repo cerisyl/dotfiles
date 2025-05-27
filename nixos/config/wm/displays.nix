d4e9991f7332a3b4648734426fdabaf4e0cbad53
Spencer Gunning <csit@cering.dev>
2025-05-27 16:40:19 -0500

    More display/RDP config

--- nixos/config/wm/displays.nix
+++ nixos/config/wm/displays.nix
@@ -7,7 +7,12 @@
       # "Default/DP-1/RefreshRate" = "${homedir}/.nix/themes/${theme}/img/bg.png";
     };
     engrit = {
-      # "Default/None-1/RefreshRate" = "${homedir}/.nix/themes/${theme}/img/bg.png";
+      # Left DP monitor
+      "Default/DP-1-1/Primary"      = true;
+      "Default/DP-1-1/RefreshRate"  = 120;
+      # Right DP monitor
+      "Default/DP-1-2/Primary"      = false;
+      "Default/DP-1-2/RefreshRate"  = 120;
     };
     astore = {
       # "Default/HDMI-0/RefreshRate" = "${homedir}/.nix/themes/${theme}/img/bg.png";
--- nixos/config/work/remmina.nix
+++ nixos/config/work/remmina.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgsUnstable, myHostname, ... }:
 if myHostname == "engrit" then {
-  services.remmina = {
+  programs.remmina = {
     enable  = true;
     package = pkgsUnstable.remmina;
   };
