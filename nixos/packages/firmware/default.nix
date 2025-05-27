bff449aa87e1d03d8e64ebfcdc5323bd0153f542
Spencer Gunning <csit@cering.dev>
2025-05-27 09:29:48 -0500

    Add displaylink

--- nixos/packages/firmware/default.nix
+++ nixos/packages/firmware/default.nix
@@ -4,6 +4,7 @@ in [
   (p "0110" false  "blueman")
   (p "0100" false  "bolt")
   (p "0100" false  "dbus")
+  (p "0100" false  "displaylink")
   (p "0100" false  "linux-firmware")
   (p "0110" false  "light")
   #(p "0100" false  "mesa")
