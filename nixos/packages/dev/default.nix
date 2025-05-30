313d0e1013b1761f4bde519ae29e0820f6d35208
Spencer Gunning <csit@cering.dev>
2025-05-30 17:51:02 -0500

    Add nodejs_24

--- nixos/packages/dev/default.nix
+++ nixos/packages/dev/default.nix
@@ -2,6 +2,7 @@
 let p = init: isUnstable: pkg: { inherit init isUnstable pkg; };
 in [
   (p "1111" true   "bun")
+  (p "0001" true   "nodejs_24") # parcel moment
   (p "1111" false  "python313")
   (p "1111" true   "vscode-fhs")
 ]
