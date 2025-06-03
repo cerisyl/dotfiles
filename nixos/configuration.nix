993d3a5d52ec32eef5eba81ae28857da1954ed77
Spencer Gunning <csit@cering.dev>
2025-06-02 20:58:13 -0500

    Add binary caches for devenv, adjust floorp searches

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -112,6 +112,12 @@ in {
     options   = "--delete-older-than 30d";
   };
 
+  # Add binary caches for devenv
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
+
   # Define packages
   nixpkgs.config.allowUnfree  = true;
   environment.systemPackages  = systemPackages;
